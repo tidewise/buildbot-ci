ca55d5f88e88169c1dcb49da4f3611badddd0488
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-03-25 13:50:28 -0300

    pull the buildbot-worker-base container instead of creating it
    It should not change often

--- containers.sh
+++ containers.sh
@@ -12,11 +12,7 @@ echo "Setting up for project ${project}"
 
 docker build -t gcr.io/$project/cache-apt containers/apt-cacher-ng
 docker build -t gcr.io/$project/cache-gem containers/gemstash
-
-docker build -t rockcore/buildbot-worker-base containers/buildbot-worker-base
-
-test -d buildbot-worker-base/buildbot && rm -rf buildbot-worker-base/buildbot
-git clone https://github.com/buildbot/buildbot buildbot-worker-base/buildbot --depth 1
+docker pull rockcore/buildbot-worker-base
 docker build -t gcr.io/$project/buildbot-worker containers/buildbot-worker
 
 docker push gcr.io/$project/cache-apt
