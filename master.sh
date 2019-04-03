a8822ef6fc71f0f3b7522cf612f7a00baaa73e6f
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-04-03 10:14:34 -0300

    change master.sh to use the gcr.io master container as built by containers.sh

--- master.sh
+++ master.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+PROJECT_NAME=$1
+if test -z "$PROJECT_NAME"; then
+    echo "Usage: master.sh PROJECT_NAME"
+    exit 1
+fi
+
 docker run -p 9989:9989 -p 8666:8666 \
     --mount type=bind,src=$PWD,dst=/var/lib/buildbot \
-    rockcore/buildbot-master
+    gcr.io/$1/buildbot-master
